5967059acbb3d45b6e367e1c3d044b1b929fcfa9
Harold Hernandez <harold@batteries911.com>
2018-03-04 18:50:02 -0500

    Create Unit Tests to networking

---
+++ MemeGeneratorTests/NetworkingTests.swift
@@ -0,0 +1,132 @@
+//
+//  MemeGeneratorTests.swift
+//  MemeGeneratorTests
+//
+//  Created by Harold Hernandez on 3/3/18.
+//  Copyright © 2018 Harold Hernandez. All rights reserved.
+//
+
+import XCTest
+@testable import MemeGenerator
+
+class NetworkingTests: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testMemesCloudCreate() {
+
+    var id: String?
+
+    let expectation = self.expectation(description: "testMemesCloudCreate")
+
+    func create(id: String) {
+
+      let request = MemesCreate(templateId: id,
+                                userName: "imgflip_hubot",
+                                password: "imgflip_hubot",
+                                textPrimary: "Primary text",
+                                textSecondary: "Secondary")
+
+      MemesCloud().create(request: request) { result in
+        switch result {
+        case let .succeeded(response):
+          XCTAssertNotNil(response, "response should not be nil")
+          XCTAssertEqual(response.success, true)
+          self.testMemesCreateResponseModel(memeCreateResponseModel: response)
+          expectation.fulfill()
+
+        case let .failed(error):
+          XCTAssertNotNil(error, "error should not be nil")
+          XCTAssertTrue(true)
+          expectation.fulfill()
+        }
+      }
+    }
+
+    MemesCloud().list { result in
+      switch result {
+      case let .succeeded(response):
+        id = response.data.memes.first?.id
+        XCTAssertNotNil(id, "response.data.memes.first?.id should not be nil")
+        create(id: id!)
+
+      case let .failed(error):
+        XCTAssertNotNil(error, "error should not be nil")
+        XCTAssertTrue(true)
+        expectation.fulfill()
+
+      }
+    }
+
+    waitForExpectations(timeout: 20) { error in
+      XCTAssertNil(error)
+    }
+  }
+
+  func testMemesCreateResponseModel(memeCreateResponseModel: MemeCreateResponseModel?) {
+
+    XCTAssertNotNil(memeCreateResponseModel, "response should not be nil")
+    XCTAssertNotNil(memeCreateResponseModel?.data, "data should not be nil")
+    XCTAssertEqual(memeCreateResponseModel?.success, true)
+    XCTAssertNotNil(memeCreateResponseModel?.data.pageUrl, "pageUrl should not be nil")
+    XCTAssertNotNil(memeCreateResponseModel?.data.url, "url should not be nil")
+  }
+
+  func testMemesCloudList() {
+
+    let expectation = self.expectation(description: "testMemesCloudList")
+
+    MemesCloud().list { result in
+      switch result {
+      case let .succeeded(response):
+        XCTAssertNotNil(response, "response should not be nil")
+        XCTAssertEqual(response.success, true)
+        self.testMemesResponseModel(memesResponseModel: response)
+        expectation.fulfill()
+
+      case let .failed(error):
+        XCTAssertNotNil(error, "error should not be nil")
+        XCTAssertTrue(true)
+        expectation.fulfill()
+
+      }
+    }
+
+    waitForExpectations(timeout: 10) { error in
+      XCTAssertNil(error)
+    }
+  }
+
+  func testMemesResponseModel(memesResponseModel: MemesResponseModel?) {
+
+    XCTAssertNotNil(memesResponseModel, "response should not be nil")
+    XCTAssertNotNil(memesResponseModel?.data, "data should not be nil")
+    XCTAssertEqual(memesResponseModel?.success, true)
+    testDataModel(dataModel: memesResponseModel?.data)
+  }
+
+  func testDataModel(dataModel: DataModel?) {
+
+    XCTAssertNotNil(dataModel, "dataModel item should not be nil")
+    let firt = dataModel?.memes.first
+    testMemesModel(memesModel: firt)
+  }
+
+  func testMemesModel(memesModel: MemesModel?) {
+
+    XCTAssertNotNil(memesModel?.id, "firt.id should not be nil")
+    XCTAssertNotNil(memesModel?.name, "firt.name should not be nil")
+    XCTAssertNotNil(memesModel?.url, "firt.url should not be nil")
+    XCTAssertNotNil(memesModel?.width, "firt.width should not be nil")
+    XCTAssertNotNil(memesModel?.height, "firt.height should not be nil")
+  }
+}
+
--- README.md
+++ README.md
@@ -5,13 +5,13 @@ A Meme Generator demo App to iOS using imgflip API
 
 [x] Initial config project and dependency manager
 [x] Config router and networking Memes API
-[  ] Tests to networking
-[  ] Create Reusable Cells Class and Nib for list memes
-[  ] Create CollectionViewController for show memes list
-[  ] Create Reusable Cells Class and Nib for details meme
-[  ] Create TableViewController for show details meme
-[  ] Enable facebook sharing
-[  ] Enable twitter sharing
-[  ] Tests integration
-[  ] Setup continuous integration
+[x] Tests to networking
+[ ] Create Reusable Cells Class and Nib for list memes
+[ ] Create CollectionViewController for show memes list
+[ ] Create Reusable Cells Class and Nib for details meme
+[ ] Create TableViewController for show details meme
+[ ] Enable facebook sharing
+[ ] Enable twitter sharing
+[ ] Tests integration
+[ ] Setup continuous integration
 
